a459c1b8aff517141852d5ef2e4eb3638a675463
Intejar <meherabintejar@gmail.com>
2023-08-18 00:42:18 +0600

    back

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const db = mysql.createConnection({
   password: "",
   database: "register",
 });
+const dbUser = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "user",
+});
+
+/* Intejar Meherab (Signin/Signup) */
 
 app.post("/register", (req, res) => {
   const sql =
@@ -38,13 +46,51 @@ app.post("/login", (req, res) => {
       return res.json("Error Hoyece");
     }
     if (data.length > 0) {
-      console.log(data);
+      console.log("data", data);
       return res.json(data);
     } else {
       return res.json("Server Error");
     }
   });
 });
+
+/* Aysha (Landlord Part) */
+// app.post("/post", (req, res) => {
+//   const sql =
+//     "INSERT INTO landlordpost (`apartment_no`,`apartment_area`,`apartment_type`,`photo_url`,`apartment_rent`,`landlord_id`,`landlord_name`,`landlord_email`,`landlord_img`) VALUES (?)";
+//   const values = [
+//     req.body.houseNo,
+//     req.body.area,
+//     req.body.type,
+//     req.body.photo_url,
+//     req.body.rent,
+//     req.body.userId,
+//     req.body.userName,
+//     req.body.userEmail,
+//     req.body.userImg,
+//   ];
+//   dbUser.query(sql, [values], (err, data) => {
+//     if (err) {
+//       return res.json("Error");
+//     }
+//     return res.json(data);
+//   });
+// });
+
+// app.post("/myPost", (req, res) => {
+//   const sql =
+//     "SELECT * FROM landlordpost WHERE `landlord_email`= ? AND `landlord_name`= ?";
+//   dbUser.query(sql, [req.body.email, req.body.name], (err, data) => {
+//     if (err) {
+//       return res.status(500).json("Error Hoyece");
+//     }
+//     if (data.length > 0) {
+//       return res.status(200).json(data);
+//     } else {
+//       return res.status(500).json("Server Error");
+//     }
+//   });
+// });
 
 app.listen(8081, () => {
   console.log("port is working");
