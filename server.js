c894020d725fe360c042bbccd6658d303f32593d
Faria20301346 <umma.faria.rahman1@g,bracu.ac.bd>
2023-08-24 12:38:20 -0700

    updated by faria

--- server.js
+++ server.js
@@ -320,6 +320,28 @@ app.post("/adminMsg", (req, res) => {
 });
 
 /*Umma Faria Rahman(Payemnt method & update) */
+app.post("/updateDeal", (req, res) => {
+  const sql = `UPDATE deal SET payment_status = ? WHERE apartment_id = ?`;
+  const values = [req.body.status, req.body.id];
+  console.log(values);
+  dbUser.query(sql, [req.body.status, req.body.id], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json(data);
+  });
+});
+app.post("/updateAdminMsg", (req, res) => {
+  const sql = `UPDATE adminmsg SET status = ? WHERE apartment_id = ?`;
+  const values = [req.body.status, req.body.id];
+  console.log(values);
+  dbUser.query(sql, [req.body.status, req.body.id], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json(data);
+  });
+});
 
 /* Payment Frontend(Intejar) */
 app.post("/create-payment-intent", async (req, res) => {
